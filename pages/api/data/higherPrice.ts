d9a2fdb977605ff4f2dcb8b6e7396c2f2f3dc9d1
jos34000 <jocelynsainson@icloud.com>
2024-11-23 22:35:26 +0100

    chore(emplacement): sectorize api calls

---
+++ pages/api/data/higherPrice.ts
@@ -0,0 +1,59 @@
+import getKlines from "@/api/binance/getKlines";
+import { validateRequest } from "@/api/validation/validateRequest";
+import { ValidationSchema } from "@/types/types";
+import logError from "@/utils/logging/logs";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const fileName = "higherPrice";
+
+const validationSchema: ValidationSchema = {
+  method: ["GET"],
+  query: {
+    crypto: { required: true, type: "string" },
+    date: { required: true, type: "string" },
+  },
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  const validation = validateRequest(req, validationSchema, fileName);
+  if (!validation.valid) {
+    return res
+      .status(validation.status || 400)
+      .json({ error: validation.error });
+  }
+
+  const { crypto, date } = validation.data!.query!;
+
+  try {
+    const response = await getKlines(crypto.toUpperCase(), date);
+    const cryptoData = response[crypto.toUpperCase()]?.EUR;
+
+    if (typeof cryptoData === "object" && cryptoData !== null) {
+      res.status(200).json({ high: cryptoData.high });
+    } else {
+      logError(
+        fileName,
+        "getKlines",
+        `Données reçues non correctes : ${JSON.stringify(cryptoData)}`
+      );
+      res.status(404).json({
+        error: "Données non trouvées",
+        crypto,
+        date,
+        response: JSON.stringify(response),
+      });
+    }
+  } catch (error) {
+    logError(
+      fileName,
+      "handler",
+      error instanceof Error ? error.message : "Erreur inconnue"
+    );
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la récupération des données" });
+  }
+}
